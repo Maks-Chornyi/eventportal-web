4e7fab8bde0837c0b129c7258aedf75ac3dba66b
Maks-Chornyi <380960310411@mail.ua>
2018-10-26 21:17:55 +0300

    dev:changed css config to scc for future components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventPageComponent } from './event-page/event-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventPageComponent,
   ],
   imports: [
     BrowserModule
