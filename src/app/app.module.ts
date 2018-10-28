7becaf4d445a5baed625b2eea64a60d2c6e92edf
Maks-Chornyi <380960310411@mail.ua>
2018-10-28 16:44:18 +0200

    dev: created home c, added routing, created card component and added view in card html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EventPageComponent } from './event-page/event-page.component';
 import { CardComponent } from './card/card.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {path: 'event', component: EventPageComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     EventPageComponent,
     CardComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
