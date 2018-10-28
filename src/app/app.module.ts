c30c90c09f5d8ee5d1734d664cec4a05f163ce0a
Maks-Chornyi <380960310411@mail.ua>
2018-10-28 14:17:23 +0200

    dev: added card component, and navbar in index.htm, added styles in app.component.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EventPageComponent } from './event-page/event-page.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventPageComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule
